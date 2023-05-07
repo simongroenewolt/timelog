c796972b1151cbfb1e70070b68e6fa038843105d
Simon Groenewolt <simon@ijsfontein.nl>
2023-05-07 23:10:16 +0200

    initial commit, basic (but working!) first version of the timelog plugin based on the vscode sample https://github.com/microsoft/vscode-extension-samples/tree/main/webview-view-sample

---
+++ media/main.js
@@ -0,0 +1,169 @@
+//@ts-check
+
+// This script will be run within the webview itself
+// It cannot access the main VS Code APIs directly.
+(function () {
+    // @ts-ignore
+    const vscode = acquireVsCodeApi();
+
+    // TODO in the future: actually use the old state -- but do we need that?
+    const oldState = vscode.getState() || { data: {} };
+
+    /**
+     * @param {number} value
+     */
+    function pad(value) {
+        if (value < 10) {
+            return "0" + value;
+        }
+        return "" + value;
+    }
+
+    /**
+     * @param {number} totalMinutes 
+     * @returns string in format hh:mm
+     */
+    function toHoursAndMinutes(totalMinutes) {
+        const hours = Math.floor(totalMinutes / 60);
+        const minutes = totalMinutes % 60;
+
+        return `${pad(hours)}:${pad(minutes)}`;
+    }
+
+    // /** @type {Array<{ value: string }>} */
+    // let colors = oldState.colors;
+
+    function selectBackward() {
+        vscode.postMessage({ type: 'moveSelection', value: 'backward' });
+    }
+
+    function selectForward() {
+        vscode.postMessage({ type: 'moveSelection', value: 'forward' });
+    }
+
+    if (document !== null) {
+        const prevButton = document.querySelector('.prev-button')
+        if (prevButton !== null) {
+            prevButton.addEventListener('click', () => {
+                selectBackward();
+            });
+        }
+        const nextButton = document.querySelector('.next-button');
+        if (nextButton !== null) {
+            nextButton.addEventListener('click', () => {
+                selectForward();
+            });
+        }
+    }
+
+    // Handle messages sent from the extension to the webview
+    window.addEventListener('message', event => {
+        const message = event.data; // The json data that the extension sent
+        switch (message.type) {
+            case 'updateProjects':
+                {
+                    updateProjects(message.data);
+                    break;
+                }
+
+        }
+        // later more?
+    });
+
+    function updateProjects(data) {
+
+        console.log('updateProjects received', data);
+
+        // add summary
+        const summaryContainer = document.querySelector('.timelog-summary');
+        if (summaryContainer !== null && data.summary !== undefined) {
+            summaryContainer.innerHTML = `<dl>
+                <dt>from</dt>
+                <dd>${data.summary.from}</dd>
+                <dt>to</dt>
+                <dd>${data.summary.to}</dd>
+            </dl>`;
+        }
+
+        // add projects
+        const container = document.querySelector('.project-list');
+        if (container !== null) {
+            container.textContent = '';
+            for (const project of data.projects) {
+                const row = document.createElement('tr');
+                row.className = 'project-entry';
+
+                // const colorPreview = document.createElement('div');
+                // colorPreview.className = 'color-preview';
+                // colorPreview.style.backgroundColor = `#${color.value}`;
+                // colorPreview.addEventListener('click', () => {
+                //     onColorClicked(color.value);
+                // });
+                // li.appendChild(colorPreview);
+
+                // const input = document.createElement('input');
+                // input.className = 'color-input';
+                // input.type = 'text';
+                // input.value = color.value;
+                // input.addEventListener('change', (e) => {
+                //     const value = e.target.value;
+                //     if (!value) {
+                //         // Treat empty value as delete
+                //         colors.splice(colors.indexOf(color), 1);
+                //     } else {
+                //         color.value = value;
+                //     }
+                //     updateColorrowst(colors);
+                // });
+
+                const projectElement = document.createElement('td');
+                const timeElement = document.createElement('td');
+                projectElement.innerText = project.name;
+                timeElement.innerText = `${toHoursAndMinutes(project.time)}`;
+                row.appendChild(projectElement);
+                row.appendChild(timeElement);
+
+                container.appendChild(row);
+            }
+        }
+
+        // add errors
+        const errorsContainer = document.querySelector('.timelog-errors');
+        if (errorsContainer !== null) {
+            errorsContainer.textContent = '';
+            if (data.errors && data.errors.length > 0) {
+                for (const errorMessage of data.errors) {
+                    const itemElement = document.createElement('div');
+                    itemElement.innerText = errorMessage;
+                    errorsContainer.appendChild(itemElement);
+                }
+            }
+        } else {
+            console.error('.timelog-errors not found');
+        }
+
+        // Update the saved state
+        vscode.setState({ data });
+    }
+    /** 
+    //  * @param {string} color 
+    //  */
+    // function onColorClicked(color) {
+    //     vscode.postMessage({ type: 'colorSelected', value: color });
+    // }
+
+    // /**
+    //  * @returns string
+    //  */
+    // function getNewCalicoColor() {
+    //     const colors = ['020202', 'f1eeee', 'a85b20', 'daab70', 'efcb99'];
+    //     return colors[Math.floor(Math.random() * colors.length)];
+    // }
+
+    // function addColor() {
+    //     colors.push({ value: getNewCalicoColor() });
+    //     updateColorList(colors);
+    // }
+}());
+
+
---
+++ src/extension.ts
@@ -0,0 +1,468 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+//
+// generic functionality
+//
+var DAYS = [
+	'Sunday',
+	'Monday',
+	'Tuesday',
+	'Wednesday',
+	'Thursday',
+	'Friday',
+	'Saturday',
+];
+
+var MONTHS = [
+	'Jan',
+	'Feb',
+	'Mar',
+	'Apr',
+	'May',
+	'Jun',
+	'Jul',
+	'Aug',
+	'Sep',
+	'Oct',
+	'Nov',
+	'Dec',
+];
+
+function pad(number: number) {
+	if (number < 10) {
+		return "0" + number;
+	}
+	return "" + number;
+}
+
+function toHoursAndMinutes(totalMinutes: number) {
+	const hours = Math.floor(totalMinutes / 60);
+	const minutes = totalMinutes % 60;
+
+	return `${pad(hours)}:${pad(minutes)}`;
+}
+
+function formatDate(now: Date) {
+	const days = DAYS;
+	const months = MONTHS;
+	return days[now.getDay()] + " " + months[now.getMonth()] + " " + pad(now.getDate()) + " " +
+		now.getFullYear() + " " +
+		pad(now.getHours()) + ":" + pad(now.getMinutes());
+}
+
+function generateMarker(now: Date) {
+	var dateMarker = "[" + formatDate(now) + "] ";
+	return dateMarker;
+}
+
+/**
+ * @param line
+ * @return object with 'date', 'originalDateString', and optional 'label' property, or undefined if no marker found
+ */
+function parseLine(line: string) {
+	if (line.startsWith('[')) {
+		var matches = line.match(/\[(.+)\]\s*([^\s]*)/);
+		if (matches !== null && matches.length === 3) {
+			var dateStamp = matches[1];
+			var projectMarker = matches[2];
+
+			// try to create a date from the dateStamp
+			var dateParts = dateStamp.match(/(\w+) (\w+) (\d+) (\d+) (\d+):(\d+)/);
+			// ["Sunday Apr 30 2023 16:36", "Sunday", "Apr", "30", "2023", "16", "36"]
+			if (dateParts !== null && dateParts.length === 7) {
+				// Note: we don't need the day in dateParts[1] to parse the date correctly
+				var monthIdx = MONTHS.indexOf(dateParts[2]);
+				var monthDay = Number.parseInt(dateParts[3], 10);
+				var year = Number.parseInt(dateParts[4], 10);
+				var hours = Number.parseInt(dateParts[5], 10);
+				var minutes = Number.parseInt(dateParts[6], 10);
+
+				var d = new Date();
+				d.setMonth(monthIdx);
+				d.setDate(monthDay);
+				d.setFullYear(year);
+				d.setHours(hours);
+				d.setMinutes(minutes);
+
+				return { date: d, label: projectMarker, originalDateString: dateStamp };
+			}
+		}
+	}
+	// no valid marker found on this line
+	return undefined;
+}
+
+function sameDate(date1: Date, date2: Date) {
+	return (
+		date1.getFullYear() === date2.getFullYear() &&
+		date1.getMonth() === date2.getMonth() &&
+		date1.getDate() === date2.getDate()
+	);
+}
+
+function processLines(lines: string[]) {
+	var firstDate = undefined;
+	var lastDate = undefined;
+	var activeProject = undefined;
+	var projects: { [key: string]: { from: Date, to: Date, duration: number }[] } = {};
+	const errors = [];
+	for (var line of lines) {
+
+		const marker = parseLine(line);
+		if (marker !== undefined) {
+			if (activeProject !== undefined) {
+				if (!projects.hasOwnProperty(activeProject)) {
+					projects[activeProject] = [];
+				}
+
+				if (lastDate !== undefined) {
+					// get difference, store time with currentProject
+
+					// check if time is reversed!
+					// add error if that is the case
+					if (marker.date.getTime() < lastDate.getTime()) {
+						errors.push(`Timestamp '${marker.originalDateString}' is not after the previous one.`);
+					}
+
+					projects[activeProject].push({
+						from: lastDate,
+						to: marker.date,
+						duration: Math.floor((marker.date.getTime() - lastDate.getTime()) / (1000 * 60)), // in minutes
+					});
+				}
+			}
+
+			if (lastDate === undefined) {
+				// store the first date ever found
+				firstDate = marker.date;
+			}
+			// store this dateStamp for the next line
+			lastDate = marker.date;
+			activeProject = marker.label;
+		}
+	}
+	console.log(projects);
+	return { firstDate, lastDate, projects, errors };
+}
+
+//
+// end generic functionality
+//
+
+class ProjectsViewProvider implements vscode.WebviewViewProvider {
+
+	public static readonly viewType = 'timelog.projects-view';
+
+	private _view?: vscode.WebviewView;
+
+	constructor(
+		private readonly _extensionUri: vscode.Uri,
+	) { }
+
+	public resolveWebviewView(
+		webviewView: vscode.WebviewView,
+		context: vscode.WebviewViewResolveContext,
+		_token: vscode.CancellationToken,
+	) {
+		this._view = webviewView;
+
+		webviewView.webview.options = {
+			// Allow scripts in the webview
+			enableScripts: true,
+
+			localResourceRoots: [
+				this._extensionUri
+			]
+		};
+
+		webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+
+		webviewView.webview.onDidReceiveMessage(data => {
+			// console.log('Extension onDidReceiveMessage ', data.type);
+			if (data.type === 'moveSelection') {
+				const editor = vscode.window.activeTextEditor;
+				if (editor !== undefined) {
+					if (editor.selections.length > 1) {
+						// warn/error!
+						vscode.window.showErrorMessage('Timelog cannot handle multiple selections');
+					} else {
+						if (data.value === 'backward') {
+							console.log('received backward message');
+
+							// move cursur to start of current selection, then search backwards
+							const newCursorPosition = new vscode.Position(
+								editor.selection.start.line,
+								Math.max(0, editor.selection.start.character - 1)
+							);
+							// start and end the same = just cursor, no selection
+							const newSelection = new vscode.Selection(newCursorPosition, newCursorPosition);
+							editor.selection = newSelection;
+							vscode.commands.executeCommand("timelog.parseAndShow");
+
+						} else {
+							// assuming forward
+							console.log('received forward message');
+
+							// move cursur to end of current selection, then search forwards
+							const newCursorPosition = new vscode.Position(
+								editor.selection.end.line,
+								editor.selection.end.character
+							);
+							// start and end the same = just cursor, no selection
+							const newSelection = new vscode.Selection(newCursorPosition, newCursorPosition);
+							editor.selection = newSelection;
+
+							vscode.commands.executeCommand("timelog.parseAndShow", 'forward');
+						}
+					}
+				} else {
+					vscode.window.showErrorMessage('Timelog no active editor found');
+				}
+			}
+		});
+	}
+
+	public updateProjects(data:
+		{
+			usingSelection: boolean,
+			errors: string[],
+			summary?: {
+				from: string,
+				to: string,
+			},
+			projects:
+			{
+				name: string,
+				time: number,
+				intervals:
+				{
+					from: Date,
+					to: Date,
+					duration: number
+				}[]
+			}[]
+		}
+	) {
+		if (this._view) {
+			this._view.show?.(true); // `show` is not implemented in 1.49 but is for 1.50 insiders
+			this._view.webview.postMessage({ type: 'updateProjects', data });
+		} else {
+			console.log('extension is missing _view');
+		}
+	}
+
+	public clearProjects() {
+		if (this._view) {
+			this._view.webview.postMessage({ type: 'clearProjects' });
+		}
+	}
+
+	private _getHtmlForWebview(webview: vscode.Webview) {
+		// Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
+		const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.js'));
+
+		// Do the same for the stylesheet.
+		const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'reset.css'));
+		const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'vscode.css'));
+		const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.css'));
+
+		// Use a nonce to only allow a specific script to be run.
+		const nonce = getNonce();
+
+		return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<!--
+					Use a content security policy to only allow loading styles from our extension directory,
+					and only allow scripts that have a specific nonce.
+					(See the 'webview-sample' extension sample for img-src content security policy examples)
+				-->
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<link href="${styleResetUri}" rel="stylesheet">
+				<link href="${styleVSCodeUri}" rel="stylesheet">
+				<link href="${styleMainUri}" rel="stylesheet">
+				<title>Cat Colors</title>
+			</head>
+			<body>
+				<div class="timelog-summary">
+				</div>
+				<div class="nav-buttons">
+				<button class="prev-button">back</button>
+				<button class="next-button">forward</button>
+			</div>
+				<table>
+					<thead>
+						<tr><th>project</th><th>time</th></tr>
+					</thead>
+					<tbody class="project-list"></tbody>
+				</table>
+				<div class="timelog-errors">
+				errors content (to be replaced)
+				</div>
+				<script nonce="${nonce}" src="${scriptUri}"></script>
+			</body>
+			</html>`;
+	}
+}
+
+function getNonce() {
+	let text = '';
+	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	for (let i = 0; i < 32; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
+}
+
+function searchSameDay(document: vscode.TextDocument, currentLineIdx: number, backward: boolean): {lineIdx: number, lines: string[]} {
+	let firstFoundDate = undefined;
+	let lines: string[] = [];
+	while (true) {
+		const line = document.lineAt(currentLineIdx).text;
+		const parsed = parseLine(line);
+		// console.log(currentLineIdx, parsed, firstFoundDate);
+		if (parsed !== undefined) {
+			if (firstFoundDate === undefined) {
+				firstFoundDate = parsed.date;
+			} else {
+				if (!sameDate(firstFoundDate, parsed.date)) {
+					// don't include this line, and stop
+					if (backward) {
+						currentLineIdx += 1;
+					} else {
+						currentLineIdx -= 1;
+					}
+					break;
+				}
+			}
+		}
+
+		// add line to lines array to be parsed (again!) later
+		if (backward) {
+			lines.unshift(line);
+			if (currentLineIdx === 0) {
+				break;
+			}
+			currentLineIdx -= 1;
+		} else {
+			lines.push(line);
+			if (currentLineIdx === document.lineCount - 1) {
+				break;
+			}
+			currentLineIdx += 1;
+		}
+		
+	}
+	return {lineIdx: currentLineIdx, lines};
+}
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('extension "timelog" is now active!');
+
+	const provider = new ProjectsViewProvider(context.extensionUri);
+
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(ProjectsViewProvider.viewType, provider));
+
+	// let disposable2 = vscode.commands.registerCommand('timelog.placeMark', () => {
+	let disposable2 = vscode.commands.registerTextEditorCommand('timelog.placeMark', (editor: vscode.TextEditor, edit) => {
+		let text = generateMarker(new Date());
+		// vscode.window.showInformationMessage('placeMark from timelog: ' + text + ' editor selections: ' + editor.selections.length);
+		// vscode.window.showInformationMessage('placeMark selections: ' + editor.selections.length);
+
+		// the user can potentially have multiple selections/cursors active, and while
+		// marking this is a bit silly, but still ok to do (parsing will _not_ work with multiple selections)
+		editor.selections.forEach((selection, i) => {
+
+			edit.insert(selection.active, text);  // insert at current cursor
+		});
+
+	});
+
+	context.subscriptions.push(disposable2);
+
+	let disposable3 = vscode.commands.registerTextEditorCommand(
+		'timelog.parseAndShow', 
+		(editor: vscode.TextEditor, edit, ...args) => {
+		// vscode.window.showInformationMessage('parseAndShow from timelog!');
+		// console.log('parseAndShow from timelog!', args
+
+		const backward = !(args.length > 0 && args[0] === 'forward');
+
+		// the user can potentially have multiple selections/cursors active
+		if (editor.selections.length > 1) {
+			vscode.window.showErrorMessage('Timelog cannot parse multiple selections');
+		}
+		let lines;
+		let usingSelection = true;
+		if (editor.selection.start.isEqual(editor.selection.end)) {
+			// console.log('no selection, just a cursor');
+
+			lines = [];
+			// no selection - try to select text for the day the cursor is _after_ by searching backwards
+			// stopping when the markers are from the previous day
+			usingSelection = false;
+			const cursorPos = editor.selection.active;
+			const document = editor.document;
+			let currentLineIdx = cursorPos.line;
+
+			const sameDay = searchSameDay(document, currentLineIdx, backward);
+
+			// TODO make 'autoselect' configurable later, or is this always the desired behavior?
+			const autoselect = true;
+			if (autoselect) {
+				// set selection
+				editor.selection = new vscode.Selection(new vscode.Position(sameDay.lineIdx, 0), cursorPos);
+			}
+			lines = sameDay.lines;
+		} else {
+			lines = editor.document.getText(editor.selection).split(/[\n\r]+/);
+
+		}
+		const processed = processLines(lines);
+		// console.log(processed);
+
+		const projects = processed.projects;
+		const firstDate = processed.firstDate;
+		const lastDate = processed.lastDate;
+		const errors = processed.errors;
+		const viewModel = Object.keys(projects).map(projectName => {
+			const intervals = projects[projectName];
+			let total = 0;
+			for (const interval of intervals) {
+				total += interval.duration;
+			}
+			return { name: projectName, time: total, intervals };
+		});
+
+		let summary = undefined;
+		if (Object.keys(projects).length > 0 && firstDate !== undefined && lastDate !== undefined) {
+			summary = {
+				from: formatDate(firstDate),
+				to: formatDate(lastDate),
+			};
+		}
+
+		provider.updateProjects({
+			usingSelection,
+			errors,
+			summary,
+			projects: viewModel,
+		});
+	});
+
+	context.subscriptions.push(disposable3);
+
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() { }
